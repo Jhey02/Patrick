bddb72dafe5a26bbd113c3f703afbe8f9ac46e39
jheyson martinez <martinezjhey65@gmail.com>
2023-08-02 13:59:40 -0500

    Prototipo

---
+++ index.js
@@ -0,0 +1,15 @@
+const carroselImages = document.querySelectorAll(".carrosel");
+
+
+carroselImages.forEach((image) => {
+    image.addEventListener("click", () => {
+        // Obtener el src de la imagen clickeada
+        const newMainFotoSrc = image.getAttribute("src");
+
+        // Obtener la imagen principal (MainFoto) por su id
+        const mainFoto = document.getElementById("MainFoto");
+
+        // Cambiar el src de la imagen principal por el de la imagen clickeada
+        mainFoto.setAttribute("src", newMainFotoSrc);
+    });
+});
